52456d48271cc64ee18b0c3fb6416dd71aea7b1a
aaronrd83 <aaronrd83@gmail.com>
2018-09-22 09:56:46 -0500

    updates to friendfinder

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,31 @@
+var friends = require('../data/friends');
+
+module.exports = function(app){
+
+    app.get('/api/friends', function (req, res) {
+        res.json(friends);
+    });
+
+    app.post('/api/friends', function(req,res){
+        var friendScores = req.body.scores;
+        var match = 0;
+        var currentDifference = 0;
+        var previousDifference = 0;
+
+        for(let i = 0; i < friends.length; i++){
+          for(let j=0; j < friendScores.length; j++){
+            currentDifference += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(friendScores[j])));
+          }
+
+          if (currentDifference < previousDifference || previousDifference === 0) {
+            previousDifference = currentDifference;
+            currentDifference = 0;
+            match = i;
+          }
+        }
+    
+        res.json(friends[match]);
+    
+        friends.push(req.body);
+    });
+}
---
+++ app/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+// SERVER
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.text());
+//app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+var date = new Date();
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+    console.log(date.toUTCString());
+  });
