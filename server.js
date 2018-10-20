1442966f42c6b51d2304ae0fe680aea8f6efe741
aaronrd83 <aaronrd83@gmail.com>
2018-10-20 14:12:11 -0500

    html path changes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
+app.use(express.static("public"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
